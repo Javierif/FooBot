fb69897c3d8891ca964e638be760df6f03dfb7d2
Carlos <nosk14@gmail.com>
2017-06-03 16:21:39 +0200

    joke service

---
+++ FooBot/FooBot/Model/JokeResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FooBot.Model
+{
+    public class JokeResponse
+    {
+        public string type { get; set; }
+        public ValueNode value { get; set; }
+    }
+
+    public class ValueNode
+    {
+        public int id { get; set; }
+        public string joke { get; set; }
+        public string[] categories { get; set; }
+    }
+}
