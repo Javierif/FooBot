82333016419d2d602f46f8d64d8d5a36b59cf993
JavierIF <javier.ifer@gmail.com>
2017-06-09 09:50:19 +0200

    Añadido el uppercase y corrección de error para imagenes

--- FooBot/FooBot/Controllers/MessagesController.cs
+++ FooBot/FooBot/Controllers/MessagesController.cs
@@ -22,7 +22,7 @@ namespace FooBot
         {
             try
             {
-                if (activity.Type == ActivityTypes.Message && activity.Text.Contains("devFooBot"))
+                if (activity.Type == ActivityTypes.Message && !string.IsNullOrEmpty(activity.Text) && activity.Text.ToUpper().Contains("devfoobot"))
                 {
                     ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
 
@@ -96,32 +96,21 @@ namespace FooBot
             return joke;
         }
 
-        private Activity HandleSystemMessage(Activity message)
+        private async Task<Activity> HandleSystemMessage(Activity message)
         {
-            if (message.Type == ActivityTypes.DeleteUserData)
+            switch (message.Type)
             {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
+                case ActivityTypes.DeleteUserData:
+                    break;
+                case ActivityTypes.ConversationUpdate:
+                    break;
+                case ActivityTypes.ContactRelationUpdate:
+                    break;
+                case ActivityTypes.Typing:
+                    break;
+                case ActivityTypes.Ping:
+                    break;
             }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
-
             return null;
         }
     }
